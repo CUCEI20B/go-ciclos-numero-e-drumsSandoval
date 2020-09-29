bef9c1f6b8f4dfff231d1327e2c45ad5b7419e38
Oscar Sandoval <drumssand@gmail.com>
2020-09-29 18:36:39 -0500

    test

--- main.go
+++ main.go
@@ -2,6 +2,27 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var n int
+	var e float64
+	e = 0
+	fmt.Scanln(&n)
+	for i := 0; i <= n; i++ {
+		if i > 1 {
+			nFactorial := factorial(i)
+			e = e + (1 / float64(nFactorial))
+		} else {
+			e = e + (1 / 1)
+		}
+	}
+	fmt.Println(e)
+
+}
+
+func factorial(n int) int {
+	factorial := n
+	for i := n; i > 1; i-- {
+		factorial = factorial * (i - 1)
+	}
+	return factorial
 }
